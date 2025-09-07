e7ad1ba941eb074c99fdaa076de18e7a7c7740b6
alexebe123 <alaabassi14@gmail.com>
2025-09-07 11:00:49 +0100

    config firebase and profile model

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:my_profile/notifiers/providers.dart';
 import 'package:my_profile/screen/home/home_screen.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:my_profile/firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
---
+++ lib/model/profile_model.dart
@@ -0,0 +1,29 @@
+class ProfileModel {
+  String name = '';
+  String id = '';
+  String imageUrl = '';
+  String yearsOfExperience = '';
+
+  ProfileModel.fromJson(Map<String, dynamic> json) {
+    try {
+      id = json['\$id'];
+    } catch (e) {}
+    try {
+      name = json['name'];
+    } catch (e) {}
+    try {
+      imageUrl = json['imageUrl'];
+    } catch (e) {}
+    try {
+      yearsOfExperience = json['yearsOfExperience'];
+    } catch (e) {}
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = {};
+    data['name'] = name;
+    data['imageUrl'] = imageUrl;
+    data['yearsOfExperience'] = yearsOfExperience;
+    return data;
+  }
+}
